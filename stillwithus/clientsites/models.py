d85efe0aebf2300dd22bb57bedb896561952c287
zkan <kan@prontomarketing.com>
2016-01-22 20:07:17 +0700

    Create ClientSite model with name and domain fields

--- stillwithus/clientsites/models.py
+++ stillwithus/clientsites/models.py
@@ -2,4 +2,16 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class ClientSite(models.Model):
+    name = models.CharField(
+        null=True,
+        blank=True,
+        max_length=500
+    )
+
+    domain = models.CharField(
+        null=True,
+        blank=True,
+        max_length=500
+    )
--- stillwithus/clientsites/tests.py
+++ stillwithus/clientsites/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import ClientSite
+
+
+class ClientSiteTest(TestCase):
+    def test_create_new_clientsite(self):
+        clientsite = ClientSite()
+        clientsite.name = 'Atlas'
+        clientsite.domain = 'www.atlasperformancechicago.com'
+
+        self.assertFalse(clientsite.id)
+        clientsite.save()
+        self.assertTrue(clientsite.id)
+
+        clientsite = ClientSite.objects.get(id=clientsite.id)
+        self.assertEqual(clientsite.name, 'Atlas')
+        self.assertEqual(clientsite.domain, 'www.atlasperformancechicago.com')
