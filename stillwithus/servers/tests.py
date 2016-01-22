e30fcc6dd5ed5281cba55a1276988d5c1c1094f9
zkan <kan@prontomarketing.com>
2016-01-22 20:49:13 +0700

    Register Server model to admin

--- stillwithus/servers/tests.py
+++ stillwithus/servers/tests.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
 from .models import Server
@@ -16,3 +17,19 @@ class ServerTest(TestCase):
         server = Server.objects.get(id=server.id)
         self.assertEqual(server.name, 'Pronto World')
         self.assertEqual(server.ip, '54.71.191.111')
+
+
+class ClientSiteAdminTest(TestCase):
+    def test_server_admin_page_should_be_accessibale(self):
+        admin = User.objects.create_superuser(
+            'admin',
+            'admin@test.com',
+            'password'
+        )
+        self.client.login(
+            username='admin',
+            password='password'
+        )
+        url = '/admin/servers/server/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
