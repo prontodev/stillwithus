e4c26436622e0705a69e746a4b9cf24e8a014133
zkan <kan@prontomarketing.com>
2016-01-24 08:39:40 +0700

    Use render method to render view

--- stillwithus/clientsites/views.py
+++ stillwithus/clientsites/views.py
@@ -4,3 +4,10 @@ from django.views.generic import TemplateView
 
 class ClientSiteView(TemplateView):
     template_name = 'clientsites.html'
+
+    def get(self, request):
+        return render(
+            request,
+            self.template_name,
+            {}
+        )
